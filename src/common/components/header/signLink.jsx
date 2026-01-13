902a905560f66e7865387aca9efdc91161862f70
bruce <bmpandrada@gmail.com>
2026-01-13 10:56:37 +0800

    fix: adjust listing form

--- src/common/components/header/signLink.jsx
+++ src/common/components/header/signLink.jsx
@@ -1,33 +1,17 @@
 import { Link } from "react-router";
 
-const SignLink = ({
-  isSticky,
-  isHome,
-  isDark,
-  handleActive,
-  customClass = "hidden lg:flex",
-}) => {
-  const singInClass =
-    !isHome || (!isSticky && isDark)
-      ? "text-dark dark:text-white"
-      : "lg:text-white";
+const SignLink = ({ isSticky, isHome, isDark, handleActive, customClass = "hidden lg:flex" }) => {
+  const singInClass = !isHome || (!isSticky && isDark) ? "text-dark dark:text-white" : "lg:text-white";
 
-  const singUpClass =
-    !isHome || (!isSticky && !isDark)
-      ? "lg:text-white signUpBtn2"
-      : "text-white signUpBtn";
+  const singUpClass = !isHome || (!isSticky && !isDark) ? "lg:text-white signUpBtn2" : "text-white signUpBtn";
 
   return (
     <div className={`${customClass}`}>
-      <Link
-        to='/signin'
-        onClick={handleActive}
-        className={`loginBtn px-[22px] py-2 text-base font-medium hover:opacity-70 ${singInClass}`}
-      >
+      <Link to='/' onClick={handleActive} className={`loginBtn px-[22px] py-2 text-base font-medium hover:opacity-70 ${singInClass}`}>
         Sign In
       </Link>
       <Link
-        to='/signup'
+        to='/'
         onClick={handleActive}
         className={` px-6 py-2 text-base font-medium duration-300 ease-in-out rounded-md bg-white/20  hover:bg-white hover:text-dark
           ${singUpClass}`}
